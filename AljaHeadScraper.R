1d071fa503ecca98457e608be217ce1a5b7cf953
Ifeanyi <72707830+Ifeanyi55@users.noreply.github.com>
2021-12-14 15:07:54 +0100

    Create AljaHeadScraper.R

---
+++ AljaHeadScraper.R
@@ -0,0 +1,15 @@
+library(rvest)
+library(tidyverse)
+
+aljurl <- read_html(paste0("https://www.aljazeera.com/"))
+
+headlinks <- aljurl %>% 
+  html_nodes(".u-clickable-card__link") %>% 
+  html_attr("href")
+
+links <- data.frame(
+  date = Sys.Date(),
+  headline_links = headlinks
+)
+
+write.csv(links,file = paste0("data/",Sys.Date(),"Headlinks",".csv"))
